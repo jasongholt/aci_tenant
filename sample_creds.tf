407eb283b30812a18edf2c4408e19a56060aea75
Jason Holt <jasonholt@godric.local>
2021-12-06 21:53:20 -0500

    added samplecreds

--- sample_creds.tf
+++ sample_creds.tf
@@ -13,8 +13,8 @@ variable "user" {
   description = "Login information"
   type        = map
   default     = {
-    username = "admin"
-    password = "!v3G@!4@Y"
-    url      = "https://sandboxapicdc.cisco.com"
+    username = ""
+    password = ""
+    url      = ""
   }
 }
