5b8acedf8c89af8e75d86109f1d3d418aceed544
Jason Holt <jason.grant.holt@gmail.com>
2022-01-20 09:17:15 -0500

    comment

--- main.tf
+++ main.tf
@@ -38,22 +38,16 @@ provider "aci" {
 module "my_tenant" {
     source                = "./modules/tenant"
     tenant_name           = "hashi-demo"
-    vrfs                  = ["vrf_a", "vrf_b"]
+    vrfs                  = ["vrf_a"]
     bridge_domains = {
       app_a_bd = {
         routing = true
         vrf     = "vrf_a"
         subnet = "192.168.10.1/24"
         scope = ["public"]
-      },
-      app_b_bd = {
-        routing = true
-        vrf     = "vrf_b"
-        subnet = "192.168.10.3/24"
-        scope = ["public"]
       }
     }
-   application_profiles = ["app_a_ap", "app_b_ap"]
+   application_profiles = ["app_a_ap"]
    epga = {
     web_epg = {
       name                = "web_epg"
@@ -66,33 +60,7 @@ module "my_tenant" {
       bridge_domain       = "app_a_bd"
     }
    }
-  epgb = {
-    web_epg = {
-      name                = "web_epg"
-      application_profile = "app_b_ap"
-      bridge_domain       = "app_b_bd"
-    },
-    db_epg = {
-      name                = "db_epg"
-      application_profile = "app_b_ap"
-      bridge_domain       = "app_b_bd"
-    }
-  }
   filters_a = {
-    filter_https = {
-      filter   = "https",
-      entry    = "https",
-      protocol = "tcp",
-      port     = "443"
-    },
-    filter_sql = {
-      filter   = "sql",
-      entry    = "sql",
-      protocol = "tcp",
-      port     = "3306",
-    }
-  }
-  filters_b = {
     filter_https = {
       filter   = "https",
       entry    = "https",
@@ -118,18 +86,6 @@ module "my_tenant" {
           filter   = "filter_sql"
         }
   }
-  contracts_b = {
-    contract_web_b = {
-          contract = "web_b",
-          subject  = "https_b",
-          filter   = "filter_https"
-        },
-    contract_sql_b = {
-          contract = "sql_b",
-          subject  = "sql_b",
-          filter   = "filter_sql"
-        }
-  }
   epga_contracts = {
     terraform_one = {
           epg           = "web_epg",
@@ -144,23 +100,6 @@ module "my_tenant" {
     terraform_three = {
           epg           = "web_epg",
           contract      = "contract_web_a",
-          contract_type = "provider" 
-    }
-  }
-  epgb_contracts = {
-    terraform_one = {
-          epg           = "web_epg",
-          contract      = "contract_sql_b",
-          contract_type = "consumer" 
-        },
-    terraform_two = {
-          epg           = "db_epg",
-          contract      = "contract_sql_b",
-          contract_type = "provider" 
-    },
-    terraform_three = {
-          epg           = "web_epg",
-          contract      = "contract_web_b",
           contract_type = "provider" 
     }
   }
